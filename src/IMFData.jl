10865ff59382854eabca66ae041331c50dcf002a
Stephen Nicar <stephenbnicar@gmail.com>
2021-04-01 09:26:09 -0500

    Updated datasets function

--- src/IMFData.jl
+++ src/IMFData.jl
@@ -10,14 +10,14 @@ const API_URL = "http://dataservices.imf.org/REST/SDMX_JSON.svc"
 
 export
     IMFSeries,
-    get_imf_datasets,
+    imf_datasets,
     get_imf_datastructure,
     get_ifs_data
 
 
 abstract type IMFSeries end
 
-include("get_imf_datasets.jl")
+include("imf_datasets.jl")
 include("get_imf_datastructure.jl")
 include("get_ifs_data.jl")
 
